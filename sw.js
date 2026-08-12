ae238c1827a1325588c9dbfb2b5f6e560963ba70
Wis-code <chiemeziem60@gmail.com>
2026-08-12 17:58:50 +0000

    Fix expense syntax and refresh app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'studiodesk-v0.6.0';
+const CACHE = 'studiodesk-v0.6.1';
 const CORE = [
   './', './index.html', './styles.css', './app.js',
   './assets/studiodesk-mark.png', './manifest.webmanifest',
